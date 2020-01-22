7aacbae8958a986e5f27fc44451d1f6ce03a36bd
Maksim Strelets <maksim.strelets98@gmailcom>
2020-01-22 15:45:46 +0200

    l_1 tasks 5, 11, 12

--- Maksim_Strelets/l_1_functions/hw/functions.py
+++ Maksim_Strelets/l_1_functions/hw/functions.py
@@ -21,6 +21,24 @@ def dict_remove(dictionaries, name):
             del el[path[0]]
 
 
+def elem_get(el, key):
+    if type(key) is not str:
+        return None
+    path = key.split(".")
+
+    if type(el) is list:
+        res = [elem_get(x, key) for x in el]
+        return res
+
+    if path[0] not in el.keys():
+        return None
+
+    if len(path) > 1:
+        return elem_get(el[path[0]], ".".join(path[1:]))
+    else:
+        return el[key]
+
+
 # возвращает по имени
 def dict_get(dictionaries, name):
     path = name.split(".")
@@ -30,14 +48,9 @@ def dict_get(dictionaries, name):
         dictionaries = [dictionaries]
 
     for el in dictionaries:
-        if not (type(el) is dict):
-            continue
-        if not (path[0] in el.keys()):
-            continue
-        if len(path) > 1:
-            res += dict_get(el[path[0]], ".".join(path[1:]))
-        else:
-            res += [el[path[0]]]
+        temp = elem_get(el, name)
+        if temp:
+            res += temp
 
     return res
 
@@ -63,18 +76,21 @@ def dfs(dictionaries):
                     if x["unit_key"] != "EUR":  keys2rem.append(x)
                 for x in keys2rem: el[key].remove(x)
             elif key == "report_name":
-                el[key] = el[key].upper()
+                if el[key] == "device":
+                    el[key] = el[key].upper()
             elif key == "page_id":
                 if el[key] == "(not set)":
                     el[key] = None
 
 
-def ins_to_dict(dictionaries, key):
+def ins_to_dict(dictionaries, key, val=""):
+    if not val:
+        val = key
     res = {}
     for i in range(len(dictionaries)):
-        if key not in dictionaries[i].keys():
+        if key not in dictionaries[i].keys() or val not in dictionaries[i].keys():
             continue
-        res[i if "id" not in dictionaries[i].keys() else dictionaries[i]["id"]] = dictionaries[i][key]
+        res[i if key not in dictionaries[i].keys() else dictionaries[i][key]] = dictionaries[i][val]
     return res
 
 
@@ -99,6 +115,40 @@ def sort(dictionaries, key):
     for el in dictionaries:
         if key not in el.keys():
             res.append(el)
+    return res
+
+
+def summary(period, api, summ, sum_level, sum_general):
+    if period > 4 or period is None:
+        period = 7
+    if sum_general == 0:
+        return None
+    formula = {
+        1: lambda: (summ * sum_level / sum_general) / period,
+        2: lambda: (summ * sum_level ^ 2 / sum_general) / period,
+        3: lambda: (sum_level / sum_general) / period,
+        4: lambda: (sum_level * 100) / period
+    }
+    return formula[api]()
+
+
+def calc_by_formula(dictionaries):
+    keys = ["period", "api", "metric_sums.sum", "metric_sums.sum_level", "metric_sums.sum_general"]
+    res = []
+    for el in dictionaries:
+        temp = {}
+        for key in keys:
+            temp[key] = elem_get(el, key)
+        if None in temp.values():
+            continue
+
+        for i in range(len(temp["metric_sums.sum"])):
+            val = summary(period=int(temp["period"]), api=int(temp["api"]),
+                          summ=int(temp["metric_sums.sum"][i]),
+                          sum_level=int(temp["metric_sums.sum_level"][i]),
+                          sum_general=int(temp["metric_sums.sum_general"][i]))
+            if val:
+                res.append(val)
     return res
 
 
--- Maksim_Strelets/l_1_functions/hw/main.py
+++ Maksim_Strelets/l_1_functions/hw/main.py
@@ -1,5 +1,5 @@
-from hw.hw_start import insights
-from hw.functions import *
+from Maksim_Strelets.l_1_functions.hw.hw_start import insights
+from Maksim_Strelets.l_1_functions.hw.functions import *
 
 removable = ["period", "count", "total_count", "page_id", "entities_affected.entities.link",
              "entities_affected.entities.status", "entities_affected.entities.days_in_data"]
@@ -15,10 +15,10 @@ removable = ["period", "count", "total_count", "page_id", "entities_affected.ent
 # temp = dict_get(insights, "objective")
 
 # task 4
-# temp = ins_to_dict(insights, "objective")
+# temp = ins_to_dict(insights, "id", "objective")
 
 # task 5
-
+# temp = ins_to_dict(dict_get(insights, "entities_affected"), "campaign_id", "campaign_name")
 
 # task 6
 # temp = list(set(ins_to_str(insights)))
@@ -34,4 +34,14 @@ removable = ["period", "count", "total_count", "page_id", "entities_affected.ent
 # insights = sort(insights, "report_name")
 # insights = sort(insights, "objective")
 
-save(insights)
+
+# task 11
+# calc_by_formula(insights)
+
+
+# task 12
+# temp = dict_get(insights, "entities_affected.entities")
+# temp = list(filter(lambda x: int(x["spend_sum"]) > 200, temp))
+
+# print(temp)
+# save(insights)
