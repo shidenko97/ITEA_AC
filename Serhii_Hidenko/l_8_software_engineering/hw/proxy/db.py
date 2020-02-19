084324de39adf46e0425db8c045771f67de2c0d5
Sergey Hidenko <shidenko97@gmail.com>
2020-02-19 21:18:26 +0200

    Eighth HW done

---
+++ Serhii_Hidenko/l_8_software_engineering/hw/proxy/db.py
@@ -0,0 +1,13 @@
+from Serhii_Hidenko.l_8_software_engineering.hw.proxy.abstract_db import AbstractDb
+
+
+class Db(AbstractDb):
+
+    def execute_query(self, query, params=None):
+        pass  # Here must be query to db
+
+    def connect(self):
+        pass  # Here must be connect to db
+
+    def disconnect(self):
+        pass  # Here must be disconnect from db
