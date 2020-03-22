25743640ae0094fc14a6b65dcfd14005d15339fe
SalvadorSakho <Salvador.Sakho21@gmail.com>
2020-03-22 13:29:28 +0200

    Fixing bugs in file to path test

--- Salvador_Sakho/l_1_functions/currying_1.py
+++ Salvador_Sakho/l_1_functions/currying_1.py
@@ -1,10 +1,11 @@
 # Currying in Python - Many to Single Argument
 
 # a(x) = b(c(d(x)))
+
+
 def change(b, c, d):
     def a(x):
         return b(c(d(x)))
-
     return a
 
 
@@ -15,13 +16,9 @@ def change(a=None):
             def y(d):
                 def z(e):
                     print(a, b, c, d, e)
-
                 return z
-
             return y
-
         return x
-
     return w
 
 
